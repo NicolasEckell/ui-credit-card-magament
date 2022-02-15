94a854353bbc737e104c8765a877a25be43f6999
niceckellHotmail <niceckell@gmail.com>
2022-02-15 00:22:40 -0300

    mvp1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import Purchase from "./Components/Purchase";
 
 function App() {
+	const [purchases, setPurchases] = useState([]);
+
+	useEffect(() => {
+		axios.get("localhost:8000/purchases").then((res) => {
+			console.log(res.data);
+			setPurchases(res.data);
+		});
+	}, []);
+
+	const purchasesMarkUp = purchases.map((item, index) => (
+		<Purchase
+			state={item.state}
+			store_name={item.store_name}
+			purchasing_date={item.purchasing_date}
+			item_name={item.item_name}
+			total_amount={item.total_amount}
+			bank={item.bank}
+			card={item.card}
+		/>
+	));
+
 	return (
-		<div className="App">
+		<div className="app">
+			{purchasesMarkUp}
 			<a
-				className=""
+				className="add-button"
 				href="https://reactjs.org"
 				target="_blank"
 				rel="noopener noreferrer"
+				title="Add new purchase"
 			>
-				Learn React
+				+
 			</a>
 		</div>
 	);
---
+++ src/Components/Purchase.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Purchase(props) {
+	return (
+		<div className="card card-item">
+			<span>{props.state}</span>
+			<span>{props.store_name}</span>
+			<span>{props.purchasing_date}</span>
+			<span>{props.item_name}</span>
+			<span>{props.total_amount}</span>
+			<span>{props.bank}</span>
+			<span>{props.card}</span>
+		</div>
+	);
+}
+
+export default Purchase;
